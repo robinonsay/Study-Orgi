f88b30a33f24d156c29cdfe110d87856da1ccd0f
raomax <raomax@gmail.com>
2015-09-12 03:24:31 -0400

    Added group class and started writing bestDate method

---
+++ Studr/Group.swift
@@ -0,0 +1,28 @@
+//
+//  Group.swift
+//  Studr
+//
+//  Created by Robin Onsay on 9/12/15.
+//  Copyright (c) 2015 JJR. All rights reserved.
+//
+
+import Foundation
+import Parse
+class Group {
+    let CLASS_NAME = "Groups"
+    func bestDate(groupID : String){
+        var query = PFQuery(className: CLASS_NAME)
+        query.getObjectInBackgroundWithId(groupID) {
+            (group: PFObject?, error: NSError?) -> Void in
+            if error == nil && group != nil {
+                println(group)
+            } else {
+                println(error)
+            }
+        }
+        var members = query["Members"] as Dictionary
+        println(members)
+        
+        
+    }
+}
