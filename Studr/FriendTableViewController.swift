7a0f3d3ed42c9ae38ce6038547c614fbfda3b187
Joseph Herkness <jpherkness@gmail.com>
2015-09-13 01:08:52 -0400

    add some stuff

--- Studr/FriendTableViewController.swift
+++ Studr/FriendTableViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class FriendTableViewController: UITableViewController ,UISearchBarDelegate, UISearchDisplayDelegate{
-
+    
     var popoverController : UIPopoverController?
+    
+    var requests = [String]()
     var friends = [String]()
+    var users = [String]()
+    var allUsers = [[String]]()
     // Current cell
     var cell : UITableViewCell?
     
@@ -19,18 +23,19 @@ class FriendTableViewController: UITableViewController ,UISearchBarDelegate, UIS
     var selectedCells = [NSIndexPath]()
     
     override init(style: UITableViewStyle) {
-    super.init(style: style);
-    friends = Database.getAcceptedFriends()
+        super.init(style: style);
+        friends = Database.getAcceptedFriends()
+        
     }
     
     override init!(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    friends = Database.getAcceptedFriends()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        friends = Database.getAcceptedFriends()
     }
     
     required init(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    friends = Database.getAcceptedFriends()
+        super.init(coder: aDecoder)
+        friends = Database.getAcceptedFriends()
     }
     /*
     self.filteredCandies = self.candies.filter({( candy: Candy) -> Bool in
@@ -38,7 +43,7 @@ class FriendTableViewController: UITableViewController ,UISearchBarDelegate, UIS
     let stringMatch = candy.name.rangeOfString(searchText)
     return categoryMatch && (stringMatch != nil)
     })
-*/
+    */
     func filterContentForSearchText(searchText: String) {
         // Filter the array using the filter method
         self.friends = self.friends.filter({( friend: String) -> Bool in
@@ -48,87 +53,89 @@ class FriendTableViewController: UITableViewController ,UISearchBarDelegate, UIS
         })
     }
     override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    // Add a back button
-    self.navigationItem.leftBarButtonItem=UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Plain, target: self, action: "backButtonTapped:")
-    
-    self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    self.tableView.tableFooterView = UIView(frame: CGRect.zeroRect)
-    
+        super.viewDidLoad()
+        
+        // Add a back button
+        self.navigationItem.leftBarButtonItem=UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Plain, target: self, action: "backButtonTapped:")
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.tableView.tableFooterView = UIView(frame: CGRect.zeroRect)
+        
     }
     
     // MARK: UITableViewDataSource
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1 // Should always be 1 in this case
+        return 3
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // Calculate number of friends from the database
-    return friends.count
+        // Calculate number of friends from the database
+        if section < allUsers.count {
+            return allUsers[section].count
+        }else{
+            return 0
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    
-    let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
-    
-    cell.textLabel!.text = friends[indexPath.row]
-    var view: UIView = UIView()
-    view.backgroundColor = UIColorFromHex(0xF68E20, alpha: 0.05)
-    cell.selectedBackgroundView = view
-    return cell;
-    
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel!.text = allUsers[indexPath.section][indexPath.row]
+        
+        var view: UIView = UIView()
+        view.backgroundColor = UIColorFromHex(0xF68E20, alpha: 0.05)
+        cell.selectedBackgroundView = view
+        return cell;
+        
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-    return 50
+        return 50
     }
     
     //MARK: UITableViewDelegate
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    
-    let cell = tableView.cellForRowAtIndexPath(indexPath)
-    
-    if (contains(self.selectedCells, indexPath)){
-    
-    cell!.accessoryType = UITableViewCellAccessoryType.None;
-    self.selectedCells = self.selectedCells.filter() { $0 !== indexPath }
-    
-    }else{
-    
-    cell!.accessoryType = UITableViewCellAccessoryType.Checkmark;
-    self.selectedCells.append(indexPath)
-    }
-    
-    print(self.selectedCells)
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        
+        if (contains(self.selectedCells, indexPath)){
+            
+            cell!.accessoryType = UITableViewCellAccessoryType.None;
+            self.selectedCells = self.selectedCells.filter() { $0 !== indexPath }
+            
+        }else{
+            
+            cell!.accessoryType = UITableViewCellAccessoryType.Checkmark;
+            self.selectedCells.append(indexPath)
+        }
+        
+        print(self.selectedCells)
     }
     
     func backButtonTapped(sender : UIButton) {
-    
-    var members = [String]()
-    for indexPath:NSIndexPath in self.selectedCells {
-    members.append(self.tableView.cellForRowAtIndexPath(indexPath)!.textLabel!.text!)
-    }
-    
-   
-    
-    if let porpOver = self.popoverController {
-    porpOver.dismissPopoverAnimated(true)
-    porpOver.delegate?.popoverControllerDidDismissPopover!(porpOver)
-    }
-    else if self.parentViewController is UINavigationController {
-    self.navigationController?.popViewControllerAnimated(true)
-    }
+        
+        var members = [String]()
+        for indexPath:NSIndexPath in self.selectedCells {
+            members.append(self.tableView.cellForRowAtIndexPath(indexPath)!.textLabel!.text!)
+        }
+        if let porpOver = self.popoverController {
+            porpOver.dismissPopoverAnimated(true)
+            porpOver.delegate?.popoverControllerDidDismissPopover!(porpOver)
+        }
+        else if self.parentViewController is UINavigationController {
+            self.navigationController?.popViewControllerAnimated(true)
+        }
     }
     
     // Generate UIColor from a HEX value
     func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
-    let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
-    let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
-    let blue = CGFloat(rgbValue & 0xFF)/256.0
-    
-    return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
     }
 }
